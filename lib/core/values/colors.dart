87e59bd619920809a166aeb75411c668ee292d87
smrtofighi <mrtofxn@gmail.com>
2022-10-26 14:25:15 -0300

    create task list page

--- lib/core/values/colors.dart
+++ lib/core/values/colors.dart
@@ -4,6 +4,7 @@ class SolidColors {
   static const Color backGround = Color(0xfffcfcfc);
   static const Color primery = Color(0xff5786ff);
   static const Color card = Color(0xffffffff);
+  static const Color backGroundTaskListPage = Color(0xffffffff);
 }
 
 class ShadowColor {
