2edefa47b2c798162e276e3b124f716121aca5fa
smrTofighi <mrtofxn@gmail.com>
2023-03-25 13:12:00 -0300

    add new string variables

--- lib/core/values/strings.dart
+++ lib/core/values/strings.dart
@@ -1,13 +1,16 @@
 class MyStrings {
+  static const String noTaskHere = 'هیچ ماموریتی اضافه نشده است';
+  static const String noTaskComplete = 'هیچ ماموریتی انجام نشده است';
+
   static const String newTask = 'ماموریت جدید';
   static const String editTask = 'ویرایش ماموریت';
   static const String add = 'افزودن';
   static const String edit = 'ویرایش';
   static const String addAlarm = 'افزودن هشدار';
-  
 
   static const String categories = 'دسته بندی ها';
   static const String all = 'همه';
 
   static const String importance = 'میزان اهمیت';
+  static const String categoryIcon = 'لوگو دسته بندی';
 }
