59a21f6e7a29e6ac903ae186b1b14ca2ed90d5d8
smrTofighi <mrtofxn@gmail.com>
2023-03-25 13:12:15 -0300

    add new icon to MyIcon Class

--- lib/core/values/icons.dart
+++ lib/core/values/icons.dart
@@ -2,15 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:todo_infinity_app/gen/assets.gen.dart';
 
 class MyIcons {
-  static ImageProvider noteBook = Image.asset(Assets.icons.notebook.path).image;
-  static ImageProvider airplan = Image.asset(Assets.icons.ariplan.path).image;
-  static ImageProvider book = Image.asset(Assets.icons.book.path).image;
-  static ImageProvider home = Image.asset(Assets.icons.home.path).image;
-  static ImageProvider music = Image.asset(Assets.icons.music.path).image;
-  static ImageProvider school = Image.asset(Assets.icons.school.path).image;
-  static ImageProvider shop = Image.asset(Assets.icons.shop.path).image;
-  static ImageProvider work = Image.asset(Assets.icons.work.path).image;
-  static ImageProvider dream = Image.asset(Assets.icons.dream.path).image;
   static ImageProvider menuVertical =
       Image.asset(Assets.icons.menuDotVertical.path).image;
   static ImageProvider arrowRight =
@@ -19,4 +10,57 @@ class MyIcons {
       Image.asset(Assets.icons.notification.path).image;
   static ImageProvider note = Image.asset(Assets.icons.notes.path).image;
   static ImageProvider tags = Image.asset(Assets.icons.tags.path).image;
+  static ImageProvider bookmarkFill =
+      Image.asset(Assets.icons.bookmarkFill.path).image;
+  static ImageProvider bookmark = Image.asset(Assets.icons.bookmark.path).image;
+  static ImageProvider pallete = Image.asset(Assets.icons.palette.path).image;
+  static ImageProvider edit = Image.asset(Assets.icons.pencil.path).image;
+  static ImageProvider power = Image.asset(Assets.icons.power.path).image;
+  static ImageProvider listCheck =
+      Image.asset(Assets.icons.listCheck.path).image;
+  static ImageProvider questionSquare =
+      Image.asset(Assets.icons.questionSquare.path).image;
+  static ImageProvider search = Image.asset(Assets.icons.search.path).image;
+  static ImageProvider setting = Image.asset(Assets.icons.settings.path).image;
+  static ImageProvider share = Image.asset(Assets.icons.share.path).image;
+  static ImageProvider sheildCheck =
+      Image.asset(Assets.icons.shieldCheck.path).image;
+  static ImageProvider sheildExcalamation =
+      Image.asset(Assets.icons.shieldExclamation.path).image;
+  static ImageProvider sun = Image.asset(Assets.icons.sun.path).image;
+  static ImageProvider sunrise = Image.asset(Assets.icons.sunrise.path).image;
+  static ImageProvider telegram = Image.asset(Assets.icons.telegram.path).image;
+  static ImageProvider like = Image.asset(Assets.icons.thumbsUp.path).image;
+  static ImageProvider trash = Image.asset(Assets.icons.trash.path).image;
+  static ImageProvider user = Image.asset(Assets.icons.user.path).image;
+  static ImageProvider world = Image.asset(Assets.icons.world.path).image;
+  //! icons for category logo
+  static ImageProvider ballot = Image.asset(Assets.icons.ballot.path).image;
+  static ImageProvider tree = Image.asset(Assets.icons.tree.path).image;
+  static ImageProvider bed = Image.asset(Assets.icons.bedAlt.path).image;
+  static ImageProvider books = Image.asset(Assets.icons.books.path).image;
+  static ImageProvider bulb = Image.asset(Assets.icons.bulb.path).image;
+  static ImageProvider cake = Image.asset(Assets.icons.cakeBirthday.path).image;
+  static ImageProvider car = Image.asset(Assets.icons.carAlt.path).image;
+  static ImageProvider dollar = Image.asset(Assets.icons.dollar.path).image;
+  static ImageProvider football = Image.asset(Assets.icons.football.path).image;
+  static ImageProvider gift = Image.asset(Assets.icons.gift.path).image;
+  static ImageProvider glassCheers =
+      Image.asset(Assets.icons.glassCheers.path).image;
+  static ImageProvider gym = Image.asset(Assets.icons.gym.path).image;
+  static ImageProvider moon = Image.asset(Assets.icons.moonStars.path).image;
+  static ImageProvider handHoldingHeart =
+      Image.asset(Assets.icons.handHoldingHeart.path).image;
+  static ImageProvider volleyball =
+      Image.asset(Assets.icons.volleyball.path).image;
+
+  static ImageProvider noteBook = Image.asset(Assets.icons.notebook.path).image;
+  static ImageProvider plane = Image.asset(Assets.icons.plane.path).image;
+  static ImageProvider book = Image.asset(Assets.icons.book.path).image;
+  static ImageProvider home = Image.asset(Assets.icons.home.path).image;
+  static ImageProvider music = Image.asset(Assets.icons.musicAlt.path).image;
+  static ImageProvider school = Image.asset(Assets.icons.school.path).image;
+  static ImageProvider shop = Image.asset(Assets.icons.shop.path).image;
+  static ImageProvider work = Image.asset(Assets.icons.work.path).image;
+  static ImageProvider dream = Image.asset(Assets.icons.dream.path).image;
 }
