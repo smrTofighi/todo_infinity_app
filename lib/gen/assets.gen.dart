60c00099d39f222cabcc175cec7e8e0125c0274c
smrTofighi <mrtofxn@gmail.com>
2023-03-25 13:11:39 -0300

    add icons

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -12,9 +12,6 @@ import 'package:flutter/widgets.dart';
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
-  /// File path: assets/icons/ariplan.png
-  AssetGenImage get ariplan => const AssetGenImage('assets/icons/ariplan.png');
-
   /// File path: assets/icons/arrow-left.png
   AssetGenImage get arrowLeft =>
       const AssetGenImage('assets/icons/arrow-left.png');
@@ -23,17 +20,76 @@ class $AssetsIconsGen {
   AssetGenImage get arrowRight =>
       const AssetGenImage('assets/icons/arrow-right.png');
 
-  /// File path: assets/icons/bell.png
-  AssetGenImage get bell => const AssetGenImage('assets/icons/bell.png');
+  /// File path: assets/icons/ballot.png
+  AssetGenImage get ballot => const AssetGenImage('assets/icons/ballot.png');
+
+  /// File path: assets/icons/bed-alt.png
+  AssetGenImage get bedAlt => const AssetGenImage('assets/icons/bed-alt.png');
 
   /// File path: assets/icons/book.png
   AssetGenImage get book => const AssetGenImage('assets/icons/book.png');
 
+  /// File path: assets/icons/bookmark-fill.png
+  AssetGenImage get bookmarkFill =>
+      const AssetGenImage('assets/icons/bookmark-fill.png');
+
+  /// File path: assets/icons/bookmark.png
+  AssetGenImage get bookmark =>
+      const AssetGenImage('assets/icons/bookmark.png');
+
+  /// File path: assets/icons/books.png
+  AssetGenImage get books => const AssetGenImage('assets/icons/books.png');
+
+  /// File path: assets/icons/bulb.png
+  AssetGenImage get bulb => const AssetGenImage('assets/icons/bulb.png');
+
+  /// File path: assets/icons/cake-birthday.png
+  AssetGenImage get cakeBirthday =>
+      const AssetGenImage('assets/icons/cake-birthday.png');
+
+  /// File path: assets/icons/car-alt.png
+  AssetGenImage get carAlt => const AssetGenImage('assets/icons/car-alt.png');
+
+  /// File path: assets/icons/car-side.png
+  AssetGenImage get carSide => const AssetGenImage('assets/icons/car-side.png');
+
+  /// File path: assets/icons/comment-info.png
+  AssetGenImage get commentInfo =>
+      const AssetGenImage('assets/icons/comment-info.png');
+
+  /// File path: assets/icons/dollar.png
+  AssetGenImage get dollar => const AssetGenImage('assets/icons/dollar.png');
+
   /// File path: assets/icons/dream.png
   AssetGenImage get dream => const AssetGenImage('assets/icons/dream.png');
 
+  /// File path: assets/icons/football.png
+  AssetGenImage get football =>
+      const AssetGenImage('assets/icons/football.png');
+
+  /// File path: assets/icons/gift.png
+  AssetGenImage get gift => const AssetGenImage('assets/icons/gift.png');
+
+  /// File path: assets/icons/github.png
+  AssetGenImage get github => const AssetGenImage('assets/icons/github.png');
+
+  /// File path: assets/icons/glass-cheers.png
+  AssetGenImage get glassCheers =>
+      const AssetGenImage('assets/icons/glass-cheers.png');
+
+  /// File path: assets/icons/gym.png
+  AssetGenImage get gym => const AssetGenImage('assets/icons/gym.png');
+
+  /// File path: assets/icons/hand-holding-heart.png
+  AssetGenImage get handHoldingHeart =>
+      const AssetGenImage('assets/icons/hand-holding-heart.png');
+
   /// File path: assets/icons/home.png
   AssetGenImage get home => const AssetGenImage('assets/icons/home.png');
+
+  /// File path: assets/icons/list-check.png
+  AssetGenImage get listCheck =>
+      const AssetGenImage('assets/icons/list-check.png');
 
   /// File path: assets/icons/menu-dot_vertical.png
   AssetGenImage get menuDotVertical =>
@@ -41,6 +97,14 @@ class $AssetsIconsGen {
 
   /// File path: assets/icons/menu.png
   AssetGenImage get menu => const AssetGenImage('assets/icons/menu.png');
+
+  /// File path: assets/icons/moon-stars.png
+  AssetGenImage get moonStars =>
+      const AssetGenImage('assets/icons/moon-stars.png');
+
+  /// File path: assets/icons/music-alt.png
+  AssetGenImage get musicAlt =>
+      const AssetGenImage('assets/icons/music-alt.png');
 
   /// File path: assets/icons/music.png
   AssetGenImage get music => const AssetGenImage('assets/icons/music.png');
@@ -56,6 +120,22 @@ class $AssetsIconsGen {
   AssetGenImage get notification =>
       const AssetGenImage('assets/icons/notification.png');
 
+  /// File path: assets/icons/palette.png
+  AssetGenImage get palette => const AssetGenImage('assets/icons/palette.png');
+
+  /// File path: assets/icons/pencil.png
+  AssetGenImage get pencil => const AssetGenImage('assets/icons/pencil.png');
+
+  /// File path: assets/icons/plane.png
+  AssetGenImage get plane => const AssetGenImage('assets/icons/plane.png');
+
+  /// File path: assets/icons/power.png
+  AssetGenImage get power => const AssetGenImage('assets/icons/power.png');
+
+  /// File path: assets/icons/question-square.png
+  AssetGenImage get questionSquare =>
+      const AssetGenImage('assets/icons/question-square.png');
+
   /// File path: assets/icons/school.png
   AssetGenImage get school => const AssetGenImage('assets/icons/school.png');
 
@@ -69,45 +149,113 @@ class $AssetsIconsGen {
   /// File path: assets/icons/share.png
   AssetGenImage get share => const AssetGenImage('assets/icons/share.png');
 
+  /// File path: assets/icons/shield-check.png
+  AssetGenImage get shieldCheck =>
+      const AssetGenImage('assets/icons/shield-check.png');
+
+  /// File path: assets/icons/shield-exclamation.png
+  AssetGenImage get shieldExclamation =>
+      const AssetGenImage('assets/icons/shield-exclamation.png');
+
   /// File path: assets/icons/shop.png
   AssetGenImage get shop => const AssetGenImage('assets/icons/shop.png');
 
   /// File path: assets/icons/sign-out.png
   AssetGenImage get signOut => const AssetGenImage('assets/icons/sign-out.png');
 
+  /// File path: assets/icons/sun.png
+  AssetGenImage get sun => const AssetGenImage('assets/icons/sun.png');
+
+  /// File path: assets/icons/sunrise.png
+  AssetGenImage get sunrise => const AssetGenImage('assets/icons/sunrise.png');
+
   /// File path: assets/icons/tags.png
   AssetGenImage get tags => const AssetGenImage('assets/icons/tags.png');
+
+  /// File path: assets/icons/telegram.png
+  AssetGenImage get telegram =>
+      const AssetGenImage('assets/icons/telegram.png');
+
+  /// File path: assets/icons/thumbs-up.png
+  AssetGenImage get thumbsUp =>
+      const AssetGenImage('assets/icons/thumbs-up.png');
+
+  /// File path: assets/icons/trash.png
+  AssetGenImage get trash => const AssetGenImage('assets/icons/trash.png');
+
+  /// File path: assets/icons/tree.png
+  AssetGenImage get tree => const AssetGenImage('assets/icons/tree.png');
 
   /// File path: assets/icons/user.png
   AssetGenImage get user => const AssetGenImage('assets/icons/user.png');
 
+  /// File path: assets/icons/volleyball.png
+  AssetGenImage get volleyball =>
+      const AssetGenImage('assets/icons/volleyball.png');
+
   /// File path: assets/icons/work.png
   AssetGenImage get work => const AssetGenImage('assets/icons/work.png');
 
+  /// File path: assets/icons/world.png
+  AssetGenImage get world => const AssetGenImage('assets/icons/world.png');
+
   /// List of all assets
   List<AssetGenImage> get values => [
-        ariplan,
         arrowLeft,
         arrowRight,
-        bell,
+        ballot,
+        bedAlt,
         book,
+        bookmarkFill,
+        bookmark,
+        books,
+        bulb,
+        cakeBirthday,
+        carAlt,
+        carSide,
+        commentInfo,
+        dollar,
         dream,
+        football,
+        gift,
+        github,
+        glassCheers,
+        gym,
+        handHoldingHeart,
         home,
+        listCheck,
         menuDotVertical,
         menu,
+        moonStars,
+        musicAlt,
         music,
         notebook,
         notes,
         notification,
+        palette,
+        pencil,
+        plane,
+        power,
+        questionSquare,
         school,
         search,
         settings,
         share,
+        shieldCheck,
+        shieldExclamation,
         shop,
         signOut,
+        sun,
+        sunrise,
         tags,
+        telegram,
+        thumbsUp,
+        trash,
+        tree,
         user,
-        work
+        volleyball,
+        work,
+        world
       ];
 }
 
