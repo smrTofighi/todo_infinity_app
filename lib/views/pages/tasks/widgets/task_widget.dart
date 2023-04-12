d336ae31b2d64fcc49c663afb4bd1cdf8db4e3fa
smrTofighi <mrtofxn@gmail.com>
2023-04-12 07:34:47 -0400

    create TaskAllWidget and TaskCompleteWidget

--- lib/views/pages/tasks/widgets/task_widget.dart
+++ lib/views/pages/tasks/widgets/task_widget.dart
@@ -1,94 +1,261 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
-
-import '../../../../controllers/category_controller.dart';
 import '../../../../controllers/task_controller.dart';
 import '../../../../core/values/colors.dart';
 import '../../../../core/values/dimens.dart';
+import '../../../../models/task_model.dart';
 
 // ignore: must_be_immutable
-class TaskWidget extends StatelessWidget {
-  TaskWidget(
-      {super.key,
-      required this.onTap,
-      required this.onLongPress,
-      required this.name,
-      required this.time,
-      required this.date,
-      required this.value,
-      required this.onChanged});
-  final String name;
-  final String time;
-  final String date;
-  final bool value;
-  final Function() onChanged;
-  final Function() onTap;
-  final Function() onLongPress;
-
+class TaskAllWidget extends StatelessWidget {
+  TaskAllWidget({super.key, required this.index});
+  final int index;
   TaskController taskController = Get.find<TaskController>();
-  CategoryController categoryController = Get.find<CategoryController>();
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      onLongPress: onLongPress,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  time,
-                  style: const TextStyle(fontSize: 14, color: Colors.grey),
-                ),
-                Text(
-                  date,
-                  style: const TextStyle(fontSize: 10, color: Colors.grey),
-                ),
-              ],
+    String time = taskController.categoryModel.value.allTaskList![index].time!;
+    String date = taskController.categoryModel.value.allTaskList![index].date!;
+    String name = taskController.categoryModel.value.allTaskList![index].name!;
+    bool value =
+        taskController.categoryModel.value.allTaskList![index].isComplete!;
+    //? return widget
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              taskController.deleteAllTask(index, context);
+            },
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(Dimens.radius),
+              bottomRight: Radius.circular(Dimens.radius),
             ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(4, 0, 12, 0),
-              margin:
-                  const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
-              height: 45,
-              width: MediaQuery.of(context).size.width / 1.55,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(Dimens.radius),
-                color: SolidColors.card,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 5,
-                    spreadRadius: 0.2,
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            backgroundColor: Colors.redAccent,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            //label: 'حذف',
+          ),
+          SlidableAction(
+            onPressed: (context) {
+              List<TaskModel> list =
+                  taskController.categoryModel.value.allTaskList!;
+              taskController.goToEditAllTask(index, list);
+            },
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(Dimens.radius),
+              bottomLeft: Radius.circular(Dimens.radius),
+            ),
+            backgroundColor: SolidColors.primary,
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            //label: 'انجام شد',
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          List<TaskModel> list =
+              taskController.categoryModel.value.allTaskList!;
+          taskController.goToEditAllTask(index, list);
+        },
+        onLongPress: () {
+          taskController.deleteAllTask(index, context);
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    name,
-                    style: const TextStyle(
-                        fontSize: 13,
-                        color: Colors.grey,
-                        decoration: TextDecoration.lineThrough),
+                    time,
+                    style: const TextStyle(fontSize: 14, color: Colors.grey),
                   ),
-                  Obx(
-                    () => Checkbox(
-                      value: value,
-                      onChanged: onChanged(),
-                      activeColor: taskController.categoryModel.value.color,
-                    ),
+                  Text(
+                    date,
+                    style: const TextStyle(fontSize: 10, color: Colors.grey),
                   ),
                 ],
               ),
-            )
-          ],
+              Container(
+                padding: const EdgeInsets.fromLTRB(4, 0, 12, 0),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                height: 45,
+                width: MediaQuery.of(context).size.width / 1.55,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimens.radius),
+                  color: SolidColors.card,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 5,
+                        spreadRadius: 0.2)
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      name,
+                      style: const TextStyle(fontSize: 13),
+                    ),
+                    Obx(
+                      () => Checkbox(
+                        value: value,
+                        onChanged: (value) {
+                          if (taskController.categoryModel.value
+                                  .allTaskList![index].isComplete ==
+                              false) {
+                            taskController.categoryModel.update((val) {
+                              val!.allTaskList![index].isComplete = true;
+                            });
+                            taskController.categoryModel.value.completeTaskList!
+                                .add(taskController
+                                    .categoryModel.value.allTaskList![index]);
+                            taskController.categoryModel.value.allTaskList!
+                                .removeAt(index);
+                          }
+                        },
+                        activeColor: taskController.categoryModel.value.color,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class TaskCompleteWidget extends StatelessWidget {
+  TaskCompleteWidget({Key? key, required this.index}) : super(key: key);
+  TaskController taskController = Get.find<TaskController>();
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {
+              taskController.deleteCompleteTask(index, context);
+            },
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(Dimens.radius),
+              bottomRight: Radius.circular(Dimens.radius),
+            ),
+            backgroundColor: Colors.redAccent,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            //label: 'حذف',
+          ),
+          SlidableAction(
+            onPressed: (context) {
+              var list = taskController.categoryModel.value.completeTaskList!;
+              taskController.goToEditCompleteTask(index, list);
+            },
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(Dimens.radius),
+              bottomLeft: Radius.circular(Dimens.radius),
+            ),
+            backgroundColor: SolidColors.primary,
+            foregroundColor: Colors.white,
+            icon: Icons.edit,
+            //label: 'انجام شد',
+          ),
+        ],
+      ),
+      child: GestureDetector(
+        onTap: () {
+          var list = taskController.categoryModel.value.completeTaskList!;
+          taskController.goToEditCompleteTask(index, list);
+        },
+        onLongPress: () {
+          taskController.deleteCompleteTask(index, context);
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 4),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    taskController
+                        .categoryModel.value.completeTaskList![index].time!,
+                    style: const TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  Text(
+                    taskController
+                        .categoryModel.value.completeTaskList![index].date!,
+                    style: const TextStyle(fontSize: 10, color: Colors.grey),
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(4, 0, 12, 0),
+                margin:
+                    const EdgeInsets.symmetric(vertical: 2.0, horizontal: 2.0),
+                height: 45,
+                width: MediaQuery.of(context).size.width / 1.55,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(Dimens.radius),
+                  color: SolidColors.card,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 5,
+                        spreadRadius: 0.2)
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      taskController
+                          .categoryModel.value.completeTaskList![index].name!,
+                      style: const TextStyle(
+                          fontSize: 13,
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                    Obx(
+                      () => Checkbox(
+                        value: taskController.categoryModel.value
+                            .completeTaskList![index].isComplete,
+                        onChanged: (value) {
+                          if (taskController.categoryModel.value
+                                  .completeTaskList![index].isComplete ==
+                              true) {
+                            taskController.categoryModel.update((val) {
+                              val!.completeTaskList![index].isComplete = false;
+                            });
+                            taskController.categoryModel.value.allTaskList!.add(
+                                taskController.categoryModel.value
+                                    .completeTaskList![index]);
+                            taskController.categoryModel.value.completeTaskList!
+                                .removeAt(index);
+                          }
+                        },
+                        activeColor: taskController.categoryModel.value.color,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
