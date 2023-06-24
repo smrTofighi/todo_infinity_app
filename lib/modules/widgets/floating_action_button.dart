6b9dc20a44ab30f87bacfc9d3b545c3f7217564d
Mohammad RezaTofighi <mrtofxn@gmail.com>
2023-06-24 18:22:41 -0400

    create new properties in the class

--- lib/modules/widgets/floating_action_button.dart
+++ lib/modules/widgets/floating_action_button.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MyFloatingActionButton extends StatelessWidget {
-  const MyFloatingActionButton({super.key, required this.onPressed,required this.color});
+  const MyFloatingActionButton(
+      {super.key, required this.onPressed, required this.color, required this.icon});
   final Function() onPressed;
-  final Color color ;
+  final Color color;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       onPressed: onPressed,
       backgroundColor: color,
-      child: const Icon(FontAwesomeIcons.plus, size: 20,),
+      child: Icon(
+        icon,
+        size: 20,
+      ),
     );
   }
 }
