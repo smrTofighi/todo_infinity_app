b5cae668386db7c4c6713bfeff437934725dc95a
Mohammad RezaTofighi <mrtofxn@gmail.com>
2023-06-24 18:04:15 -0400

    create delete category dialog

---
+++ lib/modules/category/widgets/delete_category_dialog.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_infinity_app/modules/category/category_controller.dart';
+
+import '../../../core/values/colors.dart';
+import '../../../core/values/dimens.dart';
+import '../../../core/values/strings.dart';
+
+deleteCateogryDialog(int index, BuildContext context) {
+  var width = MediaQuery.of(context).size.width;
+  CategoryController controller = Get.find<CategoryController>();
+
+  Get.defaultDialog(
+    barrierDismissible: false,
+    backgroundColor: SolidColors.card,
+    buttonColor: SolidColors.primary,
+    confirm: Container(
+      margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+      height: 30,
+      width: width / 4,
+      child: ElevatedButton(
+        onPressed: () {
+          //controller.deleteCategory(index);
+          Get.back();
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.redAccent),
+        ),
+        child: const Text(MyStrings.delete),
+      ),
+    ),
+    cancel: Container(
+      margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+      height: 30,
+      width: width / 4,
+      child: ElevatedButton(
+        onPressed: () {
+          Get.back();
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.green),
+        ),
+        child: const Text(MyStrings.cancel),
+      ),
+    ),
+   
+    title: '',
+    titleStyle: const TextStyle(fontSize: 10),
+    titlePadding: const EdgeInsets.all(0),
+    middleText: 'دسته بندی ${controller.categoryList[index].name} حذف شود؟',
+    radius: Dimens.radius,
+  );
+}
