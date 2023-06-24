cd2ff73c639c33c1a544000d0cd1ece0473b3043
Mohammad RezaTofighi <mrtofxn@gmail.com>
2023-06-24 17:56:01 -0400

    add host in variables

--- lib/core/utils/api_constant.dart
+++ lib/core/utils/api_constant.dart
@@ -1,10 +1,11 @@
 class ApiConstant {
   ApiConstant._();
   static const String host = 'http://127.0.0.1:8000/';
-  static const String getCatList = 'cat-list';
-  static const String deleteCat = 'kill-cat?id=';
+  static const String getCatList = '${host}cat-list';
+  static const String deleteCat ='${host}kill-cat?id=';
+  static const String listOfCatTodo = '${host}cat-todos?id=';
   static String newCategory(String title, String color, String icon) {
-    String url = 'new-cat?title=$title&color=$color&icon=$icon';
+    String url = '${host}new-cat?title=$title&color=$color&icon=$icon';
     return url;
   }
 }
