c36d81bfe8ff7622412f249cbb2cdd8c80df7e83
smrTofighi <mrtofxn@gmail.com>
2023-07-27 16:53:03 -0400

    add new colors in light colors class

--- lib/core/values/colors.dart
+++ lib/core/values/colors.dart
@@ -1,32 +1,42 @@
 import 'package:flutter/material.dart';
 
-class SolidColors {
+class LightColors {
+  //? text colors
+  static const Color white54Text = Colors.white54;
+  static Color greyText = Colors.grey.shade600;
+  static const Color whiteText = Colors.white;
+  static const Color blackText = Colors.black;
+  static const Color redText = Colors.red;
+
   static const Color backGround = Color(0xfffcfcfc);
   static const Color primary = Color(0xff5786ff);
   static const Color card = Color(0xffffffff);
   static const Color backGroundTaskListPage = Color(0xffffffff);
 
   static const Color grey = Colors.grey;
+
   static const Color black = Colors.black;
+
+  static const Color primaryBG = Color(0xff5786ff);
 }
 
 class ShadowColor {
   static Color black = Colors.black.withOpacity(0.1);
 }
 
-  List<Color> colorList = [
-    SolidColors.primary,
-    Colors.red,
-    Colors.green,
-    Colors.brown,
-    Colors.orange,
-    Colors.purple,
-    Colors.amber,
-    Colors.cyan,
-    Colors.lime,
-    Colors.pink,
-    Colors.teal,
-    Colors.blueGrey,
-    Colors.indigo,
-    Colors.lightGreen
-  ];
+List<Color> colorList = [
+  LightColors.primary,
+  Colors.red,
+  Colors.green,
+  Colors.brown,
+  Colors.orange,
+  Colors.purple,
+  Colors.amber,
+  Colors.cyan,
+  Colors.lime,
+  Colors.pink,
+  Colors.teal,
+  Colors.blueGrey,
+  Colors.indigo,
+  Colors.lightGreen
+];
