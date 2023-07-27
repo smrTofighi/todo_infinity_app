7b053874e3383a52d44cb92d68d4b78b94dd979e
smrTofighi <mrtofxn@gmail.com>
2023-07-27 16:56:05 -0400

    add new variables in class

--- lib/core/values/dimens.dart
+++ lib/core/values/dimens.dart
@@ -1,6 +1,13 @@
+import 'package:get/get.dart';
+
 class Dimens {
   Dimens._();
   static double infinity = double.infinity;
-  static double height = double.infinity;
+  static double width = Get.width;
+  static double height = Get.height;
   static double radius = 8;
+
+  static double small = 8;
+  static double medium = 12;
+  static double high = 16;
 }
