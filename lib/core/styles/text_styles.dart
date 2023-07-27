c1e68878c8a490959e2a742013d30543e1af8a00
smrTofighi <mrtofxn@gmail.com>
2023-07-27 16:55:34 -0400

    add new text styles in the class

--- lib/core/styles/text_styles.dart
+++ lib/core/styles/text_styles.dart
@@ -3,19 +3,47 @@ import 'package:todo_infinity_app/core/values/colors.dart';
 
 import '../../gen/fonts.gen.dart';
 
-class MyTextStyles {
-  static const TextStyle categoryTitleBlack = TextStyle(
+class LightTextStyles {
+  static TextStyle normall12(Color color) {
+    return TextStyle(fontSize: 12, color: color);
+  }
+
+  static TextStyle bold28Fanavari(Color color) {
+    return TextStyle(
+      fontSize: 28,
       fontWeight: FontWeight.bold,
-      fontSize: 20.0,
-      color: Colors.black,
-      fontFamily: FontFamily.samim);
+      color: color,
+      fontFamily: FontFamily.fanavari,
+    );
+  }
+
+  static TextStyle normal10(Color color) {
+    return TextStyle(fontSize: 10, color: color);
+  }
+
+  static const TextStyle categoryNameTitleBlack = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+    color: Colors.black,
+    fontFamily: FontFamily.samim,
+  );
+  static const TextStyle bigTitle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+    color: Colors.black,
+    fontFamily: FontFamily.samim,
+  );
+
   static const TextStyle bigTextWhite = TextStyle(
-      fontWeight: FontWeight.bold, fontSize: 28.0, color: Colors.white);
+    fontWeight: FontWeight.bold,
+    fontSize: 28.0,
+    color: Colors.white,
+  );
 
   static const TextStyle importanceTextTaskListPageComplete =
-      TextStyle(fontSize: 11, color: SolidColors.grey);
+      TextStyle(fontSize: 11, color: LightColors.grey);
   static const TextStyle importanceTextTaskListPageAll =
-      TextStyle(fontSize: 11, color: SolidColors.black);
+      TextStyle(fontSize: 11, color: LightColors.black);
 
   static const TextStyle titleOfAppBar = TextStyle(
     color: Colors.black,
